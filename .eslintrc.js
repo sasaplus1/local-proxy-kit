1e215e8c44304fc352860b7173076c1caa432a7b
sasaplus1 <sasaplus1@gmail.com>
2019-01-10 00:13:29 +0900

    added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  root: true
+};
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,4 @@
 module.exports = {
+  '*.js': 'npm run lint',
   'package.json': ['npm run fixpack', 'git diff --exit-code --quiet']
 };
