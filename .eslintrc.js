6ddcf8b38decfd56a91bbddf194334bf11e2e1fa
sasaplus1 <sasaplus1@gmail.com>
2019-01-19 22:53:39 +0900

    added eslint rule for Proxy Auto Configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,32 @@ module.exports = {
         mocha: true
       },
       files: ['test/**/*.js']
+    },
+    {
+      files: ['test/**/*.pac'],
+      env: {
+        browser: true,
+        node: false
+      },
+      globals: {
+        // NOTE: http://findproxyforurl.com/pac-functions/
+        alert: true,
+        dateRange: true,
+        dnsDomainIs: true,
+        dnsDomainLevels: true,
+        dnsResolve: true,
+        isInNet: true,
+        isPlainHostName: true,
+        isResolvable: true,
+        localHostOrDomainIs: true,
+        myIpAddress: true,
+        shExpMatch: true,
+        timeRange: true,
+        weekdayRange: true
+      },
+      rules: {
+        'no-unused-vars': ['error', { varsIgnorePattern: 'FindProxyForURL' }]
+      }
     }
   ],
   parserOptions: {
